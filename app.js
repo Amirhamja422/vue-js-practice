0c6a692bcb615d002275e9255bf0f4e6789c3fca
Amirhamja422 <amirhamja422@gmail.com>
2023-08-17 01:52:23 +0600

    data print

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 new Vue({
     el: "#app",
     data: {
-        title: "AMir Hamja"
+        title: "AMir Hamja",  // string print
+        isItTrue : true,
+        robot :{
+            name :"franky"
+        },
+        
+        cars :[
+            'ford', 'toyota'
+        ],
+
+        greeting :()=>{
+            return "hello world";
+        }
+
     }
 })
