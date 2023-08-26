9dd6a7676c38888c8507262ab64c66356fcf798a
Amirhamja422 <amirhamja422@gmail.com>
2023-08-26 19:10:20 +0600

    <p v-else >

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ new Vue({
         //     name :"franky"
         // },
 
-        src: "https://picsum.photos/200/300",
-        alt: "This is boss",
-        link: "https://www.google.com/",
-        myText: "This is bol",
-        myHtml: "<strong>Hello Warld</strong"
+        // src: "https://picsum.photos/200/300",
+        // alt: "This is boss",
+        // link: "https://www.google.com/",
+        // myText: "This is bol",
+        // myHtml: "<strong>Hello Warld</strong"
         // cars :[
         //     'ford', 'toyota'
         // ],
@@ -20,21 +20,45 @@ new Vue({
         //     return "hello world";
         // }
 
+
+        // ## Conditional Rendering part 07
+
+        user: 'Amir Hamja',
+        userAge: 27,
+        allowedAge: 28
+
     },
 
-    methods: {
-        
-        greeting(){
-            // return "hello world";
+    // ## Conditional Rendering part 07
+ 
+    methods: {   
+        changeUser(){
 
-            return this.title;
+            return this.user==='Amir Hamja'? true : false;
         }
+    }
+     
+
+   // ## method use in vue js
+
+    // methods: {
+
+        // ## 1st process
+        
+        // greeting(){
+        //     // return "hello world";
+
+        //     return this.title;
+        // }
+
+         // ## 2nd process
+
 
         // greeting : function(){
         //     return "hello world";
         // }
 
-    }
+    // }
 
 
 
