3a6e319d3404df73d0444698d7845cfe5a7b5cb9
Amirhamja422 <amirhamja422@gmail.com>
2023-10-29 14:33:38 +0600

    component

--- app.js
+++ app.js
@@ -1,180 +1,182 @@
 // const ap11 = new Vue({
-    // el: "#app",
-    // data: {
-        // title: "AMir Hamja",  // string print
-        // isItTrue : true,
-        // robot :{
-        //     name :"franky"
-        // },
+// el: "#app",
+// data: {
+// title: "AMir Hamja",  // string print
+// isItTrue : true,
+// robot :{
+//     name :"franky"
+// },
 
-        // src: "https://picsum.photos/200/300",
-        // alt: "This is boss",
-        // link: "https://www.google.com/",
-        // myText: "This is bol",
-        // myHtml: "<strong>Hello Warld</strong"
-        // cars :[
-        //     'ford', 'toyota'
-        // ],
+// src: "https://picsum.photos/200/300",
+// alt: "This is boss",
+// link: "https://www.google.com/",
+// myText: "This is bol",
+// myHtml: "<strong>Hello Warld</strong"
+// cars :[
+//     'ford', 'toyota'
+// ],
 
-        // greeting :()=>{
-        //     return "hello world";
-        // }
+// greeting :()=>{
+//     return "hello world";
+// }
 
+// ## Conditional Rendering part 07
 
-        // ## Conditional Rendering part 07
+// user: 'Amir',
+// x : 0,
+// y : 0,
 
-        // user: 'Amir',
-        // x : 0,
-        // y : 0,
+//         value:"1st"
+//     },
 
-    //         value:"1st"
-    //     },
+//     methods:{
+//         changeValue(){
+//          ap12.value ='changed';
+//         }
+//     }
+// });
 
-    //     methods:{
-    //         changeValue(){
-    //          ap12.value ='changed';
-    //         }
-    //     }
-    // });
+// const ap12 = new Vue({
+//     el: "#app2",
+//         data: {
+//             value:"2nd"
+//         },
 
-    // const ap12 = new Vue({
-    //     el: "#app2",
-    //         data: {
-    //             value:"2nd"
-    //         },
-      
-    
-    // methods: {
-    //     addToA() {
-    //       return this.a + this.salary;
-    //     },
-    //     addToB() {
-    //         return this.b + this.salary;
-    
-    //     }
-    //   }
+// methods: {
+//     addToA() {
+//       return this.a + this.salary;
+//     },
+//     addToB() {
+//         return this.b + this.salary;
 
+//     }
+//   }
 
-   
+// userAge: 27,
+// allowedAge: 28,
 
-
-        // userAge: 27,
-        // allowedAge: 28,
-
-        // cars : ['one', 'two', 'three'],
+// cars : ['one', 'two', 'three'],
 // obj crete
-        // users: {
-        //     name: 'amir',
-        //     age:27,
-        //     profession: 'web develop'
-        // },
+// users: {
+//     name: 'amir',
+//     age:27,
+//     profession: 'web develop'
+// },
 
-   
+// methods: {
+//  update(){
+//     setTimeout(() =>{
+//     this.user ='test'
+//     },2000)
+//  },
+//  getCord(event){
+//     this.x = event.clientX;
+//     this.y = event.clientY;
 
-   
-        // methods: {
-        //  update(){
-        //     setTimeout(() =>{
-        //     this.user ='test'
-        //     },2000)
-        //  },
-        //  getCord(event){
-        //     this.x = event.clientX;
-        //     this.y = event.clientY;
-    
-        //     }
-        // },
+//     }
+// },
 
-        // methods: {
-        //     update(){
-        //        setTimeout(() =>{
-        //        this.user ='test'
-        //        })
-        //     }
-        //    },
+// methods: {
+//     update(){
+//        setTimeout(() =>{
+//        this.user ='test'
+//        })
+//     }
+//    },
 
-    
+// methods: {
+//     update(newame,event){
+//        this.user =newame;
+//        console.log(event);
 
-        // methods: {
-        //     update(newame,event){
-        //        this.user =newame;
-        //        console.log(event);
-            
-        //     }
-        //    },
+//     }
+//    },
 
-           
-        // methods: {
-        //    handleForm(){
-        //     //event.preventDefault();
-        //     console.log(this.formData);
+// methods: {
+//    handleForm(){
+//     //event.preventDefault();
+//     console.log(this.formData);
 
-        //    }
-        // }
+//    }
+// }
 
-    // ## Conditional Rendering part 07
- 
-    // methods: {   
-    //     changeUser(){
+// ## Conditional Rendering part 07
 
-    //         return this.user==='Amir Hamja'? true : false;
-    //     }
-    // }
-     
+// methods: {
+//     changeUser(){
 
-   // ## method use in vue js
+//         return this.user==='Amir Hamja'? true : false;
+//     }
+// }
 
-    // methods: {
+// ## method use in vue js
 
-        // ## 1st process
-        
-        // greeting(){
-        //     // return "hello world";
+// methods: {
 
-        //     return this.title;
-        // }
+// ## 1st process
 
-         // ## 2nd process
+// greeting(){
+//     // return "hello world";
 
+//     return this.title;
+// }
 
-        // greeting : function(){
-        //     return "hello world";
-        // }
+// ## 2nd process
 
-    // }
+// greeting : function(){
+//     return "hello world";
+// }
 
-
+// }
 
 // });
 
 
 
 // <!--  ## Part: 17   Mount & Template  (Part: 17)-->
-const temp = `<p>this is a value two {{ value }}</p>`;
+// const temp = `<p>this is a value two {{ value }}</p>`;
 
-const ap13 = new Vue({
-    // el: "#app3",
-        data: {
-            value:"2nd"
-        },
+// const ap13 = new Vue({
+//   // el: "#app3",
+//   data: {
+//     value: "2nd",
+//   },
 
-        // template:`<p>this is a value two {{ value }}</p>`
-        template:temp
+//   // template:`<p>this is a value two {{ value }}</p>`
+//   template: temp,
+// });
+// //$mount hlo built in method
+// setTimeout(() => {
+//   ap13.$mount("#app3");
+// }, 2000);
+
+// <!--  ## Part: 17   Mount & Template  (Part: 17)-->
+
+
+
+
+// <!--  ## Part: 18  Use of Basic Component  (Part: 18)-->
+
+
+Vue.component('test', {
+    data() {
+      return {
+        name: 'amir',
+      };
+    },
   
-    });
-//$mount hlo built in method
-    setTimeout(()=> {
-        ap13.$mount('#app3')
+    template: `<p>{{name}}</p>`,
+  })
+  
+  new Vue({
+    el: "#app"
+  });
+  
 
 
-    },2000);
-
-    // <!--  ## Part: 17   Mount & Template  (Part: 17)-->
+// <!--  ## Part: 18   Use of Basic Component (Part: 18)-->
 
 
 
 
-
-
-    
 // directive 2 types er hoyea thakea
